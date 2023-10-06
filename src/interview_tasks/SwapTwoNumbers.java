1b9ae169bda6223e6534ece9abf3819f13af21c5
abdelali20223 <abdelalielgarnaoui1993@gmail.com>
2023-10-06 17:15:33 -0400

    SwapTwoNumbers task

---
+++ src/interview_tasks/SwapTwoNumbers.java
@@ -0,0 +1,23 @@
+package interview_tasks;
+
+public class SwapTwoNumbers {
+
+
+        public static void main(String[] args) {
+            int a = 12 ;//21
+            int b = 33 ;
+
+            a = b-a;
+            b-=a;//12
+            a+=b;//33
+
+
+            System.out.println(a);
+            System.out.println(b);
+            //
+
+
+        }
+    }
+
+
